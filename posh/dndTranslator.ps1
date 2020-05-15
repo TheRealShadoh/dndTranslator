3e413b3dc4a7381facbb1ecab1be8b5377b0fdf5
therealshadoh <christian.crill@outlook.com>
2020-05-14 22:24:12 -0400

    init

---
+++ posh/dndTranslator.ps1
@@ -0,0 +1,35 @@
+# Setup players
+$players = @() #Array to hold all the players
+
+$player1 = @{
+    charName = 'Shadoh'
+    playerName = 'Chris'
+    charRace = 'Elf'
+    charLanguages = @(
+        'Common',
+        'Elven'
+    )
+}
+$player2 = @{
+    charName = 'Exeldrian'
+    playerName = 'Pete'
+    charRace = 'Dwarf'
+    charLanguages = @(
+        'Common',
+        'Dwarven'
+    )
+}
+$player2 = @{
+    charName = 'Zeirith'
+    playerName = 'Kody'
+    charRace = 'Half-Elf'
+    charLanguages = @(
+        'Common',
+        'Elven',
+        'Goblin'
+    )
+}
+
+# Input
+
+# Do the thing
