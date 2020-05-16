ebca142fcddf4db51a0cb2bcf92afd2af8ce13d4
therealshadoh <christian.crill@outlook.com>
2020-05-16 02:42:41 -0400

    gui refactor for basic json. needs path mapping

--- dndTranslatorPOSH/dndTranslatorPOSH/dndTranslatorPOSH_PlayerScreen.ps1
+++ dndTranslatorPOSH/dndTranslatorPOSH/dndTranslatorPOSH_PlayerScreen.ps1
@@ -1,16 +1,25 @@
-﻿$button3_Click = {
+﻿
+$button3_Click = {
 	$richtextbox4.text = Get-TranslatedMessage -key $textbox2.text -message $richtextbox3.text 
 }
 
-$button4_Click = {
+$button4_Click = { #copy
 	[System.Windows.Forms.Clipboard]::SetText($richTextBox2.Text)
 }
 
 $button2_Click = {
-	$richtextbox2.text = Set-TranslatedMessage -key $textbox1.text -message $richtextbox1.text
+	$richtextbox1.text = Get-TranslatedMessageAuto -LanguageFile $global:langMap -message $richtextbox1.text
 }
 
 . (Join-Path $PSScriptRoot 'dndTranslatorPOSH_PlayerScreen.designer.ps1')
-Import-Module .\modules\dndTranslator.psm1 -Force
+Import-Module  C:\git\dndTranslator\dndTranslatorPOSH\dndTranslatorPOSH\modules\dndTranslator.psm1 -Force -Verbose
+$global:langMap = Get-Content C:\git\dndTranslator\data\languages\default.json -Raw
+$global:langMap = $global:langMap | ConvertFrom-Json
+
+# Setup Players
+$global:players = Get-childitem C:\git\dndTranslator\data\playerFiles\ | ForEach-Object {Get-Content $_.FullName -Raw | ConvertFrom-Json}   #will need to set this up for proper pathing
+
+# Setup NPC
+$global:npcs = Get-childitem C:\git\dndTranslator\data\npcFiles\ | ForEach-Object {Get-Content $_.FullName -Raw | ConvertFrom-Json}   #will need to set this up for proper pathing
 
 $dndTranslatorPOSH_PlayerScreen.ShowDialog()
